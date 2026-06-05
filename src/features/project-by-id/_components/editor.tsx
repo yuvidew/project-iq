5785fd639f9c313555516a61099bc6acc07167a0
Yuvraj dewangan <yd00102@gmail.com>
2026-06-05 21:07:22 +0530

    feat: refactor Editor component for improved readability and upload image handling

--- src/features/project-by-id/_components/editor.tsx
+++ src/features/project-by-id/_components/editor.tsx
@@ -5,38 +5,46 @@ import { useCreateBlockNote } from "@blocknote/react";
 import "@blocknote/core/fonts/inter.css";
 import "@blocknote/mantine/style.css";
 import { useTheme } from "next-themes";
-import {  useMemo } from "react";
+import { useMemo } from "react";
 import { darkTheme, lightTheme } from "../lib";
+import { useUploadImage } from "@/features/image/hooks/use-upload-image-hook";
 
 interface EditorProps {
-    value : string;
-    onChange : (value : string) => void;
+    value: string;
+    onChange: (value: string) => void;
     isEditable?: boolean;
 }
 
 export const Editor = ({ value, onChange, isEditable = true }: EditorProps) => {
     const { resolvedTheme } = useTheme();
+    const { mutateAsync: uploadImage } = useUploadImage();
 
     const theme = useMemo(() => {
         return resolvedTheme === "dark" ? darkTheme : lightTheme;
     }, [resolvedTheme]);
 
+    const handleUploadFile = async (file: File): Promise<string> => {
+        const result = await uploadImage({ file });
+        return result.url;
+    }
+
     const editor = useCreateBlockNote({
         initialContent:
             value
                 ? JSON.parse(value) : undefined,
+        uploadFile : handleUploadFile
     });
 
     return (
-            <BlockNoteView
-                editor={editor}
-                editable={isEditable}
-                theme={theme}
-                onChange={() => {
-                    onChange(JSON.stringify(editor.document, null, 2));
-                }}
-                className="min-h-[200px] [&_.bn-container]:max-w-full [&_.bn-editor]:max-w-full [&_.bn-block-content]:break-words [&_.bn-inline-content]:break-words [&_pre]:overflow-x-auto [&_pre]:max-w-full [&_table]:max-w-full [&_table]:overflow-x-auto"
+        <BlockNoteView
+            editor={editor}
+            editable={isEditable}
+            theme={theme}
+            onChange={() => {
+                onChange(JSON.stringify(editor.document, null, 2));
+            }}
+            className="min-h-[200px] [&_.bn-container]:max-w-full [&_.bn-editor]:max-w-full [&_.bn-block-content]:break-words [&_.bn-inline-content]:break-words [&_pre]:overflow-x-auto [&_pre]:max-w-full [&_table]:max-w-full [&_table]:overflow-x-auto"
 
-            />
+        />
     )
 }
